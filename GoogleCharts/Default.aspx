ff7a72c4471f10f6d5263fd881f908243e4f2c06
Mudassar Tufail <mudassartufail@live.com>
2018-05-09 23:00:54 +0500

    GoogleCharts with ASP.NET and C#

---
+++ GoogleCharts/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GoogleCharts.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Google Charts with ASP.NET / C#</title>
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <!--Bar Chart Div that will hold the bar chart-->
+            <div id="bar_chart_div"></div>
+            
+            <!--Pie Chart Div that will hold the pie chart-->
+            <div id="pie_chart_div"></div>
+        </div>
+    </form>
+    <!--Literals for binding script on front end from code behind -->
+    <asp:Literal ID="pieChartScript" runat="server"></asp:Literal>
+    <asp:Literal ID="barChartScript" runat="server"></asp:Literal>
+</body>
+</html>
---
+++ GoogleCharts/Default.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Data;
+using System.Text;
+
+namespace GoogleCharts
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadPieChart();
+                LoadBarChart();
+            }
+        }
+
+        //Create Google Pie Chart
+        protected void LoadPieChart()
+        {
+            DBClass objDB = new DBClass(); //Database Class for dynamic data
+            DataTable recTable = new DataTable();   
+            recTable=objDB.GetDataTable(); //Get Table from database
+
+            StringBuilder chartString = new StringBuilder();
+            chartString.Append(" <script type='text/javascript'>");
+
+            chartString.Append(" google.charts.load('current', { 'packages': ['corechart'] });");
+            chartString.Append(" google.charts.setOnLoadCallback(drawChart);");
+            
+            chartString.Append(" function drawChart() {");
+            chartString.Append(" var data = new google.visualization.DataTable();");
+            chartString.Append(" data.addColumn('string', 'Province');");
+            chartString.Append(" data.addColumn('number', 'Population');");
+            chartString.Append(" data.addRows([");
+
+            foreach (DataRow row in recTable.Rows)
+            {
+                foreach (DataColumn column in recTable.Columns)
+                {
+                    chartString.Append(" ['" + column.ColumnName + "', "+row[column.ColumnName]+"],");
+                }
+            }
+            
+            chartString.Append(" ]);");
+            chartString.Append(" var options = {");
+            chartString.Append(" 'title': 'Pakistan Population',");
+            chartString.Append(" is3D: true,");
+            chartString.Append(" 'width': 800,");
+            chartString.Append(" 'height': 400");
+            chartString.Append(" };");
+
+            chartString.Append(" var chart = new google.visualization.PieChart(document.getElementById('pie_chart_div'));");
+            chartString.Append(" chart.draw(data, options);");
+            chartString.Append(" }");
+            chartString.Append(" </script>");
+
+            pieChartScript.Text = chartString.ToString();  //Bind Script to front end literals
+        }
+
+        //Load Google Bar Chart
+        protected void LoadBarChart()
+        {
+            DBClass objDB = new DBClass(); //Database Class for dynamic data
+            DataTable recTable = new DataTable();
+            recTable = objDB.GetDataTable(); //Get Table from database
+
+            StringBuilder chartString = new StringBuilder();
+            chartString.Append(" <script type='text/javascript'>");
+
+            chartString.Append(" google.load('visualization', '1', {packages:['columnchart']});");
+            chartString.Append(" google.setOnLoadCallback(drawChart);");
+
+            chartString.Append(" function drawChart() {");
+            chartString.Append(" var data = google.visualization.arrayToDataTable([");
+            chartString.Append(" ['Province', 'Population'],");
+
+            foreach (DataRow row in recTable.Rows)
+            {
+                foreach (DataColumn column in recTable.Columns)
+                {
+                    chartString.Append(" ['" + column.ColumnName + "', " + row[column.ColumnName] + "],");
+                }
+            }
+            
+            chartString.Append(" ]);");
+
+            chartString.Append(" var chart = new google.visualization.ColumnChart(document.getElementById('bar_chart_div'));");
+            chartString.Append(" chart.draw(data, {width: 600, height: 300, is3D: true, title: 'Pakistan Population'});");
+            chartString.Append(" }");
+            chartString.Append(" </script>");
+
+            barChartScript.Text = chartString.ToString(); //Bind Script to front end literals
+        }
+    }
+}
